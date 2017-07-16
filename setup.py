bdb42f56a840213a9f74ff2b54d2b19c81a6e977
Alexandre Boukhlif <blokfyuh@users.noreply.github.com>
2017-07-16 15:05:27 -0300

    Changed project_name

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
 
-project_name = 'youtube-api-samples'
+project_name = 'youtube_api_samples'
+
 setup(
     name=project_name,
     url='https://github.com/blokfyuh/api-samples',
-    packages=['youtube-api-samples'],
-    package_dir={'youtube-api-samples': 'python'}
+    packages=[project_name],
+    package_dir={project_name: 'python'}
 )
